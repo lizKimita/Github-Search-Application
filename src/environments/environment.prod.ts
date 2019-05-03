0cdcccf8468046358902831a04ec7c759f65f169
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-05-03 08:12:15 +0300

    Delete environment.prod.ts

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl:"https://api.github.com/users/",
-  accessToken:"?access_token=125bd1682550988de7f398d8050bad5786a31059",
-  repos:"/repos"
-};
